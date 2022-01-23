2ecb5d7cc4ec497fc0fd181ef3aa51e0f58759f8
developeranaz_BOT <96877776+dev-extended@users.noreply.github.com>
2022-01-23 15:46:42 +0530

    Create rclone.sh

---
+++ scripts/rclone.sh
@@ -0,0 +1,1 @@
+|grep --colour 'DownloadCandidate:' |sed 's/DownloadCandidate:/\nFILE="/g' |sed 's/@/"\n/g' |grep 'FILE="' |sort -u |sed 's|$|; rclone copy "/jdx/jdownload/$FILE" "$(cat /remote)":jd2rc --config=/rclone.conf; rclone copy "/jdx/$FILE" "$(cat /remote)":jd2rc --config=/rclone.conf|g'
