ad1ee2d484de254ebee3f33bebe424686f643670
developeranaz_BOT <96877776+dev-extended@users.noreply.github.com>
2022-01-23 15:45:33 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,5 +4,7 @@ cp /Jdownloader-2-Rclone-Heroku/jd.sh /jd.sh
 chmod +x /jd.sh
 cp /Jdownloader-2-Rclone-Heroku/serve.sh /serve.sh
 chmod +x /serve.sh
+cp /Jdownloader-2-Rclone-Heroku/scripts/rclone.sh /rclone.sh
+chmod +x /rclone.sh
 cat /Jdownloader-2-Rclone-Heroku/cr.json |sed "s|THEJDUSERPASSWORD|$JD_USER_PASSWORD|g" |sed "s|THEJDUSEREMAIL|$JD_USER_EMAIL|g" >"/cfg/org.jdownloader.api.myjdownloader.MyJDownloaderSettings.json"
 supervisord -c /Jdownloader-2-Rclone-Heroku/supervisord.conf
